3b1f554a5d57272e1757f78b353544c526f5c127
Rosa Elena Muñoz Ugalde <relena.periodista@gmail.com>
2024-12-13 07:32:54 -0600

    Update app.py

--- app.py
+++ app.py
@@ -7,8 +7,4 @@ st.title("Rosaelena Muñoz Ugalde")
 st.write("""
 Este proyecto explora los datos de la base de datos **Sakila**, una base de datos ficticia utilizada comúnmente en tutoriales y ejemplos de SQL. Contiene información sobre el alquiler de películas, clientes, empleados, pagos y más.  
 El objetivo de este análisis es explorar dinámicas clave del negocio a través de visualizaciones interactivas, como gráficos de dispersión y análisis de rentas por categorías, con el fin de ofrecer insights estratégicos que apoyen la toma de decisiones comerciales.
- 
-
-### Propósito del Proyecto  
-Este proyecto tiene como objetivo realizar un análisis visual utilizando los datos de la base de datos sakila con el fin de identificar patrones y relaciones clave en el negocio de alquiler de películas. 
 """)
