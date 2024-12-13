f13af75615c6da24c9083960269deb726ff5890c
Rosa Elena Muñoz Ugalde <relena.periodista@gmail.com>
2024-12-12 20:51:21 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,28 @@
 import streamlit as st
-import pandas as pd
-import sqlite3
 import plotly.express as px
-from data.load_data import load_sakila_data
 
-# Configuración inicial
-st.set_page_config(page_title="Dashboard Sakila", layout="wide")
 
-# Título de la app
-st.title("Análisis de la Base de Datos Sakila")
-st.markdown("""
-## Rosaelena Muñoz Ugalde
-## Proyecto Final
-## Este dashboard Interactivo permite analizar y visualizar datos sobre el comportamiento de los alquileres de películas.
+st.title("Proyecto Final Programación III")  
+st.title("Rosaelena Muñoz Ugalde")
+st.write("""
+Este proyecto explora los datos de la base de datos **Sakila**, una base de datos ficticia utilizada comúnmente en tutoriales y ejemplos de SQL. Contiene información sobre el alquiler de películas, clientes, empleados, pagos y más.  
+El objetivo de este análisis es explorar dinámicas clave del negocio a través de visualizaciones interactivas, como gráficos de dispersión y análisis de rentas por categorías, con el fin de ofrecer insights estratégicos que apoyen la toma de decisiones comerciales.
+ 
+
+### Propósito del Proyecto  
+A través de los siguientes análisis visuales, se busca:
+- **Relación entre pagos y empleados**: Utilizando un gráfico de dispersión, 
+se analiza cómo los pagos de los clientes están relacionados con los empleados encargados, 
+lo que puede ayudar a entender el desempeño de los empleados en función de las ventas.
+  
+- **Distribución de rentas por tienda y categoría**: A través de un gráfico de barras, 
+se examina la cantidad total de rentas por tienda y por categoría de películas, 
+lo que permite identificar qué tiendas y qué tipos de películas son más populares, optimizando las estrategias de inventario y marketing.  
+
+- **Análisis de montos de pago por cliente**: Analizando cómo se distribuyen los pagos entre los clientes, se pueden identificar patrones de consumo y clientes con mayor potencial de compra repetitiva.
+
+Estos análisis no solo ayudan a entender las tendencias de alquiler y pago, 
+sino que también permiten identificar áreas de mejora para tomar decisiones estratégicas en cuanto a ventas, marketing e inventario.
+
+Mi enfoque está en aplicar el análisis de datos para resolver problemas comerciales y generar valor estratégico en el entorno de alquiler de películas.
 """)
-
-# Agregar una imagen local desde la carpeta 'assets/images/'
-st.image("/Users/rosaelenamunoz/Documents/sakila_dashboard/imagen.png")
